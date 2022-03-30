626fd3fc049d52fab85e1c9d360244ba4ede809f
ivyhu <ajianouc@gmail.com>
2022-03-30 12:39:34 -0700

    set up example.env & server to add authentication header

--- server/index.js
+++ server/index.js
@@ -1,1 +1,34 @@
-// TBD
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
+
+
+const app = express();
+
+// Serves up all static and generated assets in ../client/dist.
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
+
+// set up get method and post method
+app.get('/*', (req, res) => {
+
+  const { method, params, data } = req
+  const url = req.headers.url.toString();
+  console.log("updated url ", url)
+  axios({
+    url,
+    method,
+    params,
+    data,
+    headers: {'Authorization' : `${process.env.GITHUB_APIKEY}`}
+  })
+  .then(({ data }) => {
+    // console.log("data is", data);
+    res.send(data)
+  })
+  .catch(console.log)
+});
+
+app.listen(process.env.PORT);
+console.log(`Listening at http://localhost:${process.env.PORT}`);
