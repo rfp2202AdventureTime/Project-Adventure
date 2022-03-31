ef5cbce4e1863148b7930c2fcf2e283f3b198886
Walter Latimer <walter@walterlatimer.com>
2022-03-30 18:07:29 -0700

    Add babel plugin for styled-components

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+
 import Overview from './components/Overview/Overview.jsx';
 import QA from './components/QA/QA.jsx';
 import Ratings from './components/Ratings/Ratings.jsx';
 import RelatedProducts from './components/RelatedProducts/RelatedProducts.jsx';
 
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 3em;
+`;
+
 const App = () => {
   return (
     <>
-    <h1>hello world</h1>
-    <Overview />
-    <RelatedProducts />
-    <QA />
-    <Ratings />
+      <Title >
+        Adventure Time
+      </Title>
+      <Overview />
+      <RelatedProducts />
+      <QA />
+      <Ratings />
     </>
   );
 };
 
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
