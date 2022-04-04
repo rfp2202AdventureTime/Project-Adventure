66c2082734958a208b997be525a31225c5d5f7b4
Walter Latimer <walter@walterlatimer.com>
2022-04-04 11:15:24 -0700

    Add top-level component HTML and CSS for Overview section

---
+++ client/src/components/Overview/ImageGallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styled from 'styled-components';
+
+function ImageGallery() {
+  return (
+    <>
+      Image Gallery
+    </>
+  );
+}
+
+export default ImageGallery;
---
+++ client/src/components/Overview/StyleSelector.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styled from 'styled-components';
+
+function StyleSelector() {
+  return (
+    <div>
+      Style Selector
+    </div>
+  );
+}
+
+export default StyleSelector;
