d8eb693eaf4160596d272a9aed41e0b51dfca000
ivyhu <ajianouc@gmail.com>
2022-04-03 18:19:39 -0700

    Add axios call for reviewMeta

--- client/src/components/Ratings/RatingBreakdown.jsx
+++ client/src/components/Ratings/RatingBreakdown.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import RatingOverview from './RatingOverview';
 
+export const ReviewMetaContext = React.createContext();
 export default function RatingBreakdown() {
+  const [reviewMeta, setReviewMeta] = useState('null');
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'http://localhost:3000/reviews/meta',
+      params: {
+        product_id: '65632',
+      },
+    })
+      .then(({ data }) => {
+        setReviewMeta(data);
+      });
+  }, []);
+
   return (
     <RatingsSection>
-      <RatingOverview />
-      {/* <RatingList />
-      <FactorList /> */}
+      <ReviewMetaContext.Provider value={reviewMeta}>
+        <RatingOverview />
+        {/* <RatingList />
+        <FactorList /> */}
+      </ReviewMetaContext.Provider>
     </RatingsSection>
   );
 }
