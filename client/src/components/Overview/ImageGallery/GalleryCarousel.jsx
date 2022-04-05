d8cbb87044355977fede40bd3ad0528403807da2
Walter Latimer <walter@walterlatimer.com>
2022-04-04 23:44:16 -0700

    Limit gallery carousel viewport dynamically

--- client/src/components/Overview/ImageGallery/GalleryCarousel.jsx
+++ client/src/components/Overview/ImageGallery/GalleryCarousel.jsx
@@ -4,32 +4,46 @@ import styled from 'styled-components';
 
 import GalleryThumbnail from './GalleryThumbnail';
 
-function GalleryCarousel({ photos, activeIndex }) {
+function GalleryCarousel({ photos, activeIndex, maxSize }) {
   return (
-    <Carousel>
-      {photos.map((photo, i) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <GalleryThumbnail isSelected={(activeIndex === i)} key={i} url={photo.url} />
-      ))}
-    </Carousel>
+    <CarouselViewport size={maxSize}>
+      <Carousel>
+        {photos.map((photo, i) => (
+          <GalleryThumbnail
+            // eslint-disable-next-line react/no-array-index-key
+            key={i}
+            isSelected={(activeIndex === i)}
+            url={photo.url}
+          />
+        ))}
+      </Carousel>
+    </CarouselViewport>
   );
 }
 
 const Carousel = styled.div`
+  width: 100%;
+`;
+
+const CarouselViewport = styled.div`
   display: inline-block;
   width: 72px;
   position: absolute;
   top: 20px;
   left: 20px;
+  height: ${((props) => props.size * 85)}px;
+  overflow: hidden;
 `;
 
 GalleryCarousel.propTypes = {
   photos: PropTypes.arrayOf(PropTypes.shape()).isRequired,
   activeIndex: PropTypes.number,
+  maxSize: PropTypes.number,
 };
 
 GalleryCarousel.defaultProps = {
   activeIndex: 0,
+  maxSize: 3,
 };
 
 export default GalleryCarousel;
