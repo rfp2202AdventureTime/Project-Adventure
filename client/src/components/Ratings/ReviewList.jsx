a58d0013216a2da6c1e45e806947127f48b3b221
ivyhu <ajianouc@gmail.com>
2022-04-05 16:12:33 -0700

    Set up basic file structure at tier 2 for rating

--- client/src/components/Ratings/ReviewList.jsx
+++ client/src/components/Ratings/ReviewList.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ReviewProvider } from '@Contexts/ReviewContext';
+import ReviewTile from './Review/ReviewTile';
 
 export default function ReviewList() {
   return (
     <RatingContainer>
       <ReviewProvider>
         ReviewList
+        <ReviewTile />
       </ReviewProvider>
     </RatingContainer>
   );
