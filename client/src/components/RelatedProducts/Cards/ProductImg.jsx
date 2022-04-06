50209b26d760f8021d18b4303f5dfdf2e466344f
Ryan De Leon <ryandeleon0@gmail.com>
2022-04-06 15:34:34 -0700

    added simple modal window when 'compare' button is clicked. It only displays inline with the parent <div>. Will need to change to absolute position eventually

--- client/src/components/RelatedProducts/Cards/ProductImg.jsx
+++ client/src/components/RelatedProducts/Cards/ProductImg.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import { React, useState } from 'react';
 import styled from 'styled-components';
-// import Comparison from './Comparison';
+import Comparison from './Comparison';
 
 const FavButton = styled.button`
-  color: tomato;
+  color: black;
   position: relative;
   float: right;
 `;
 
+const TestingModal = styled.section`
+  display: ${(props) => props.show ? 'fixed' : 'none'};
+  position: inline;
+  background: green;
+  z-index: 500;
+  border-style: 10px solid;
+`;
+
+// link should take it to the product page (productID)
+// src should image of related products
 function ProductImg({ image }) {
-  // link should take it to the product page (productID)
-  // src should image of related products
-  const productThumbnail = image.results[0].photos[0].thumbnail_url
+  const [showModal, setShowModal] = useState(false);
+  const productThumbnail = image.results[0].photos[0].thumbnail_url;
+
+  function handleClick() {
+    setShowModal(!showModal);
+  }
 
   return (
     <div id="parent">
-      <FavButton as="a" href="http://bing.com">Compare</FavButton>
+      <TestingModal show={showModal}>
+        <Comparison />
+      </TestingModal>
+      {/* <FavButton as="a" href="http://bing.com">Compare</FavButton> */}
+      <FavButton onClick={() => handleClick()}>Compare</FavButton>
       <a href="http://google.com">
         <img
           src={productThumbnail}
--- client/src/components/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedProducts/RelatedProducts.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import sampleData from '../../sampleData';
-import { Individualcard, IndCard } from './Cards/Individualcard';
 import Outfit from './Outfit';
 import { RelatedProvider } from './RelatedContext';
-
 import AllCards from './Cards/AllCards';
 
 function RelatedProducts() {
@@ -20,10 +17,6 @@ function RelatedProducts() {
       <h1>Your Outfit</h1>
       <Layout>
         <Outfit />
-        {/* <IndCard>
-          <ProductImg />
-          <Individualcard />
-        </IndCard> */}
 
       </Layout>
     </RelatedProductsSection>
