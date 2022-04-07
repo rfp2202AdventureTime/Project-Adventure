de6c3f850cdb7c88a4709033168556c2b3881195
Walter Latimer <walter@walterlatimer.com>
2022-04-07 12:50:17 -0700

    Add deep zoom for expanded view

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import { useCurrentProduct } from '@Contexts/ProductIDContext';
@@ -12,21 +12,11 @@ import ProductDescription from './ProductDescription';
 function Overview() {
   const { activeStyle } = useActiveStyle();
   const { currentProduct } = useCurrentProduct();
-  const [view, setView] = useState('default');
-
-  const handleViewChange = (e, newView) => {
-    if (e.target === e.currentTarget) setView(newView);
-  };
+  const photos = activeStyle ? activeStyle.photos : [];
 
   return (
     <>
-      <ExpandedImageGallery>
-        <DefaultImageGallery>
-          <ImageGalleryViewport className={view}>
-            {activeStyle && <ImageGallery view={view} handleViewChange={handleViewChange} />}
-          </ImageGalleryViewport>
-        </DefaultImageGallery>
-
+      <ImageGallery photos={photos}>
         <ProductInfo>
           <Category>{currentProduct ? currentProduct.category : ''}</Category>
           <ProductName>{currentProduct ? currentProduct.name : 'Product Loading'}</ProductName>
@@ -34,38 +24,12 @@ function Overview() {
           {activeStyle && <StyleSelector />}
           <AddToCart />
         </ProductInfo>
-      </ExpandedImageGallery>
+      </ImageGallery>
 
       <ProductDescription />
     </>
   );
 }
-
-const ExpandedImageGallery = styled.section`
-  background-color: ${(props) => props.theme.colors.light};
-  display: flex;
-  height: 630px;
-`;
-
-const DefaultImageGallery = styled.div`
-  width: 800px;
-  overflow: visible;
-  z-index: 2;
-`;
-
-const ImageGalleryViewport = styled.div`
-  background-color:${(props) => props.theme.colors.background};
-  width: 100%;
-  height: 100%;
-  &.expanded {
-    width: ${() => document.getElementById('main').offsetWidth}px;
-    transition: width 1s ease-in-out;
-  }
-  &.default {
-    width: 100%;
-    transition: width 1s ease-in-out;
-  }
-`;
 
 const ProductInfo = styled.section`
   background-color:${(props) => props.theme.colors.light};
