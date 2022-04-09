56e22bc15e208568dd939fa12f2acd29191291f1
Walter Latimer <walter@walterlatimer.com>
2022-04-09 00:30:50 -0700

    Recover some of Add to Cart

--- client/src/components/Overview/AddToCart.jsx
+++ client/src/components/Overview/AddToCart.jsx
@@ -39,7 +39,7 @@ function AddToCart({ skus }) {
       </SelectSize>
 
       <SelectQuantity onChange={handleQuantity}>
-        <option>Quantity</option>
+        <option>NO STOCK</option>
         {[...Array(5).keys()].map((num) => (
           <option
             value={num + 1}
@@ -58,12 +58,38 @@ function AddToCart({ skus }) {
 }
 
 const AddToCartButton = styled.button`
+  appearance: none;
+  padding: 15px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  background: ${({ theme }) => theme.colors.light};
+  font-size: 0.8em;
+  text-transform: uppercase;
+  text-align: left;
+  font-weight: bold;
+  margin: 5px;
+  width: 280px;
+  cursor: pointer;
 `;
 
-const SelectSize = styled.select`
+const CustomSelect = styled.select`
+  appearance: none;
+  padding: 15px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  background: ${({ theme }) => theme.colors.light};
+  font-size: 0.8em;
+  text-transform: uppercase;
+  align: left;
+  font-weight: bold;
+  margin: 5px;
+  outline: none;
 `;
 
-const SelectQuantity = styled.select`
+const SelectSize = styled(CustomSelect)`
+  width: 150px;
+`;
+
+const SelectQuantity = styled(CustomSelect)`
+  width: 120px;
 `;
 
 AddToCart.propTypes = {
