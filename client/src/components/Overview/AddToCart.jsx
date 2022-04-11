3e6698ee662d4e14bf2c8814ef0a9e92d7eba8cf
Walter Latimer <walter@walterlatimer.com>
2022-04-11 14:24:54 -0700

    Complete Add to Cart features

--- client/src/components/Overview/AddToCart.jsx
+++ client/src/components/Overview/AddToCart.jsx
@@ -2,85 +2,84 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-// This isn't fully done. Things left to do:
-// - Disable the style selector when no items are in stock
-// - Remove the Add to Cart and Quantity Selectors when out of stock
-// - Default options for Size and Quantity selectors aren't correct.
 // - Actually make the POST request when adding to cart instead of console logging.
 // - Down arrow styling for the dropdowns.
 
 function AddToCart({ skus }) {
-  const [currentSku, setCurrentSku] = useState();
+  const [selectedSku, setSelectedSku] = useState();
   const [quantity, setQuantity] = useState();
-  const [stock, setStock] = useState();
   const [error, setError] = useState(false);
-  const [validSkus, setValidSkus] = useState([]);
 
-  useEffect(() => {
-    const valid = Object.keys(skus)
-      .filter((sku) => (skus[sku].quantity > 0))
-      .map((sku) => ({ ...skus[sku], sku }));
-    setValidSkus(valid);
-  }, [skus]);
+  const inStockSkus = Object.keys(skus)
+    .filter((sku) => (skus[sku].quantity > 0))
+    .map((sku) => ({ ...skus[sku], sku }));
+
+  // Assume nothing is in stock. If it is, return that or 15, whichever is lower.
+  let currentStock = 0;
+  if (skus[selectedSku]) {
+    currentStock = (skus[selectedSku].quantity > 15)
+      ? 15
+      : skus[selectedSku].quantity;
+  }
 
   useEffect(() => {
     setQuantity(0);
     setError(false);
-    if (skus[currentSku]) {
-      setStock(skus[currentSku].quantity > 15 ? 15 : skus[currentSku].quantity);
-    }
-  }, [currentSku]);
+  }, [selectedSku]);
 
   const handleChange = (e) => {
-    setCurrentSku(e.target.value);
+    if (skus[e.target.value]) {
+      setSelectedSku(e.target.value);
+    } else {
+      setSelectedSku(null);
+    }
   };
 
-  const handleQuantity = (e) => {
-    setQuantity(e.target.value);
-  };
+  const handleQuantity = (e) => setQuantity(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!currentSku) {
+    if (!selectedSku) {
       setError(!error);
     } else {
       // eslint-disable-next-line no-console
-      console.log(`Size: ${skus[currentSku].size} - Quantity: ${quantity}`);
+      console.log(`Size: ${skus[selectedSku].size} - Quantity: ${quantity}`);
     }
   };
+
+  const anySkus = inStockSkus.length > 0;
 
   return (
     <>
       <SelectionError>{error ? 'Please select a size!' : null}</SelectionError>
-      <SelectSize onChange={handleChange}>
-        {validSkus.length > 0
-          ? (validSkus.map((sku) => (
-            <option
-              value={sku.sku}
-              key={sku.sku}
-            >
-              {sku.size}
-            </option>
-          )))
-          : <option disabled>NO STOCK</option>}
+
+      <SelectSize defaultValue="selectsize" onChange={handleChange}>
+        {anySkus
+          ? (
+            <>
+              <option value="selectsize">SELECT SIZE</option>
+              {(inStockSkus.map((sku) => (
+                <option value={sku.sku} key={sku.sku}>{sku.size}</option>)))}
+            </>
+          )
+          : <option value="selectsize">NO STOCK</option>}
       </SelectSize>
 
-      <SelectQuantity onChange={handleQuantity}>
-        {!stock
-          ? <option>NO STOCK</option>
-          : [...Array(stock).keys()].map((num) => (
-            <option
-              value={num + 1}
-              key={num + 1}
-            >
-              {num + 1}
-            </option>
-          ))}
-      </SelectQuantity>
+      {anySkus
+        ? (
+          <>
+            <SelectQuantity defaultValue="selectQuantity" onChange={handleQuantity}>
+              {!currentStock
+                ? <option value="selectquantity">-</option>
+                : [...Array(currentStock).keys()].map((num) => (
+                  <option value={num + 1} key={num + 1}>{num + 1}</option>
+                ))}
+            </SelectQuantity>
 
-      <AddToCartButton onClick={handleSubmit}>
-        Add to Cart
-      </AddToCartButton>
+            <AddToCartButton onClick={handleSubmit}>Add to Cart</AddToCartButton>
+          </>
+        )
+        : null}
     </>
   );
 }
