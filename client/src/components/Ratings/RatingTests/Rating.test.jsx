d00456be8f98d9ca8f6b03b0deeeae30a033674d
ivyhu <ajianouc@gmail.com>
2022-04-11 21:56:01 -0700

    add snapshot test

--- client/src/components/Ratings/RatingTests/Rating.test.jsx
+++ client/src/components/Ratings/RatingTests/Rating.test.jsx
@@ -2,25 +2,27 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
+import renderer from 'react-test-renderer';
 import FactorEntry from '../RatingBreakdown/FactorEntry';
 
+import RatingOverview from '../RatingBreakdown/RatingOverview';
 
-test('use jsdom in this test file', () => {
-  const element = document.createElement('div');
-  expect(element).not.toBeNull();
+// Snapshot Testing
+it('RatingOverview renders correctly', () => {
+  const tree = renderer
+    .create(<RatingOverview />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
-
-
+// Rendering
 let container = null;
 beforeEach(() => {
-  // setup a DOM element as a render target
   container = document.createElement('div');
   document.body.appendChild(container);
 });
 
 afterEach(() => {
-  // cleanup on exiting
   unmountComponentAtNode(container);
   container.remove();
   container = null;
@@ -30,6 +32,23 @@ it('renders with factorBar descriptions', () => {
   act(() => {
     render(<FactorEntry factor="Size" />, container);
   });
-  // expect(container.textContent[0]).toBe('Too small');
   expect(container.textContent).toBe('Too smallPerfectToo large');
+
+  act(() => {
+    render(<FactorEntry factor="Comfort" />, container);
+  });
+  expect(container.textContent).toBe('PoorPerfect');
 });
+
+// Data Fetching
+it("renders user data", async () => {
+  const fakeUser = {
+    name: "Joni Baez",
+    age: "32",
+    address: "123, Charming Avenue"
+  };
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(fakeUser)
+    })
+  );
