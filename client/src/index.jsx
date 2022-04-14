9e56b6c68b376ed49ff0615147119cccd4bc4d87
Ryan De Leon <ryandeleon0@gmail.com>
2022-04-13 23:13:53 -0700

    cleaned up index.js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -14,7 +14,7 @@ import RelatedProducts from './components/RelatedProducts/RelatedProducts';
 
 function App() {
   const [defaultProductId, setDefaultProductId] = useState(65636);
-  // tests: 65655, 65633, 65654
+
   return (
     <Theme>
       <ProductProvider productId={defaultProductId}>
