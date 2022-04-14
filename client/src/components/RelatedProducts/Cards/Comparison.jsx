a05f946accd70fe9b243e9bd9b5f2753bebc94bf
Ryan De Leon <ryandeleon0@gmail.com>
2022-04-14 00:14:10 -0700

    changed the format of comparison table so that feature is in the center and values are on the right. If feature's value is null, 'true' is appended

--- client/src/components/RelatedProducts/Cards/Comparison.jsx
+++ client/src/components/RelatedProducts/Cards/Comparison.jsx
@@ -14,7 +14,7 @@ function Comparison() {
 
     // creates an array with all the features
     const FeatArray = item1.features.concat(item2.features);
-
+console.log(FeatArray, 'featarray')
     // create a set for stringified objects
     const stagingSet = new Set();
 
@@ -49,11 +49,13 @@ function Comparison() {
         <tbody>
           {FeatureSet.map((item, key) => (
             <tr key={key}>
-              <Xfeature>{item1Array.map((item1) => (item1.feature === item.feature ? '✓' : ' '))}</Xfeature>
+              <Xfeature>{item1Array.map((item1) => (item1.feature === item.feature ? (item1.value) ? `${item1.value}` : 'True' : ' '))}</Xfeature>
 
-              <Value>{item.value ? `${item.value}` : `${item.feature}`}</Value>
+              {/* <Value>{item.value ? `${item.value}` : `${item.feature}`}</Value> */}
+              <Value>{item.feature}</Value>
 
-              <Xfeature>{item2Array.map((item2) => (item2.feature === item.feature ? '✓' : ' '))}</Xfeature>
+              <Xfeature>{item2Array.map((item2) => ((item2.feature === item.feature) ? (item2.value) ? `${item2.value}` : 'True' : ' '))}</Xfeature>
+
             </tr>
           ))}
 
